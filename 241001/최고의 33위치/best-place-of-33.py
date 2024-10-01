6817010480691da0adf24b561f9a3072e21f0cea
박주영 <143913325+Juyoung1520@users.noreply.github.com>
2024-10-01 16:01:51 +0900

    Title: 최고의 33위치, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241001/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,23 @@
+n = int(input())
+grid = []  
+
+for i in range(n):
+    # 입력받은 문자열을 리스트에 추가
+    grid.append(list(map(int, input().split())))  # 각 입력을 정수 리스트로 변환하여 추가
+
+
+def get_num_of_gold(row, col_s, col_e):
+    num_of_gold = 0
+
+    for col in range(col_s, col_e+1):
+        num_of_gold += grid[row][col]
+    return num_of_gold
+
+max_gold = 0
+for row in range(n):
+    for col in range(n):
+        if col+2 >= n:
+            continue
+        
+        max_gold = max(max_gold, get_num_of_gold(row, col, col+2))
+print(max_gold)
