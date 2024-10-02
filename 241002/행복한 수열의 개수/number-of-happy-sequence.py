5600788da8d68806b07b35f7106586adfd08251a
박주영 <143913325+Juyoung1520@users.noreply.github.com>
2024-10-02 14:11:48 +0900

    Title: 행복한 수열의 개수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241002/행복한 수열의 개수/number-of-happy-sequence.py
+++ 241002/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,41 +1,37 @@
 n, m = map(int,input().split())
 grid =[[*map(int,input().split())] for _ in range(n)]
 
-
 sum = 0
 
 for row in range(n):
-    count = 0
+    
     for col in range(n):
-        if col+1 >= n:
+        if col+1 == n:
             continue
+        count = 1
         k = grid[row][col]
         if k != grid[row][col+1]:
-            k = grid[row][col+1]
-            count = 0 #리셋
-        elif k == grid[row][col+1]:
-            if count >= m-1:
-                continue
+            continue
+        else:
             count += 1
-
-    sum += count
+    if count >= m:
+        sum += 1
         
 
-
+        
 for col in range(n):
-    count = 0
+
     for row in range(n):
-        if row+1 >= n:
+        if row+1 == n:
             continue
+        count = 1
         k = grid[row][col]
         if k != grid[row+1][col]:
-            k = grid[row+1][col]
-            count = 0
-        elif k == grid[row+1][col]:
-            if count >= m-1:
-                continue
+            continue
+        else:
             count += 1
+    if count >= m:
+        sum += 1
             
-    sum += count
 
 print(sum)
