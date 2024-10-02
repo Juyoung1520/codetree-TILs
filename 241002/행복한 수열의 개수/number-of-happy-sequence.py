4e226bf9eb25d28993b196df5d685e77fb409607
박주영 <143913325+Juyoung1520@users.noreply.github.com>
2024-10-02 16:14:38 +0900

    Title: 행복한 수열의 개수, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

--- 241002/행복한 수열의 개수/number-of-happy-sequence.py
+++ 241002/행복한 수열의 개수/number-of-happy-sequence.py
@@ -2,29 +2,31 @@ n, m = map(int,input().split())
 grid =[[*map(int,input().split())] for _ in range(n)]
 
 happy_sequences = 0
+if n == 1:
+    happy_sequences = 2 if m == 1 else 0
+else:
+    for row in range(n):
+        count = 1
+        for col in range(1,n):
+            if grid[row][col-1] == grid[row][col]:
+                count += 1
+            else:
+                count = 1
+            if count >= m:
+                happy_sequences += 1
+                break
 
-for row in range(n):
-    count = 1
-    for col in range(1,n):
-        if grid[row][col-1] == grid[row][col]:
-            count += 1
-        else:
-            count = 1
-        if count >= m:
-            happy_sequences += 1
-            break
-
-     
-for col in range(n):
-    count = 1
-    for row in range(1,n):
-        if grid[row-1][col] == grid[row][col]:
-            count += 1
-        else:
-            count = 1
-        if count >= m:
-            happy_sequences += 1
-            break
+        
+    for col in range(n):
+        count = 1
+        for row in range(1,n):
+            if grid[row-1][col] == grid[row][col]:
+                count += 1
+            else:
+                count = 1
+            if count >= m:
+                happy_sequences += 1
+                break
 
 
 
