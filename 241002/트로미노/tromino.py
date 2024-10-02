775f601e953c25e13b3861648596f7dea1c5c7d4
박주영 <143913325+Juyoung1520@users.noreply.github.com>
2024-10-02 17:39:15 +0900

    Title: 트로미노, Time: 130ms, Memory: 25MB, Status: Passed - CodeTree

--- 241002/트로미노/tromino.py
+++ 241002/트로미노/tromino.py
@@ -6,14 +6,14 @@ grid = [
 
 max_value = 0
 for i in range(1,n):
-    for j in range(1,n):
+    for j in range(1,m):
         max_value = max(max_value, grid[i-1][j-1] + grid[i-1][j] + grid[i][j], grid[i][j-1] + grid[i-1][j] + grid[i][j], grid[i][j-1] + grid[i-1][j-1] + grid[i][j], grid[i-1][j-1] + grid[i][j-1] + grid[i-1][j])
 
 for i in range(n):
-    for j in range(2,n):
+    for j in range(2,m):
         max_value = max(max_value, grid[i][j-2]+grid[i][j-1]+grid[i][j])
 
-for j in range(n):
+for j in range(m):
     for i in range(2,n):
         max_value = max(max_value, grid[i-2][j]+grid[i-1][j]+grid[i][j])
 
